b80bdde6cb0a3bc7fa9fd746399e24c687910544
Camile Campos <camilecampos25@gmail.com>
2019-08-29 20:37:00 -0300

    exercícios 01 e 02

---
+++ aula-20190829/atividade02.cs
@@ -0,0 +1,17 @@
+using System;
+namespace PacotePrograma {
+    class Programa {
+        static void Main (string[] args) {
+            int n1;
+            Console.WriteLine ("Informe um valor inteiro: ");
+            x = int.Parse (Console.ReadLine ());
+
+            if (x > 0) {
+                Console.WriteLine ("Não negativo");
+            } else {
+                Console.WriteLine ("Negativo")
+            }
+            Console.ReadKey();
+        }
+    }
+}
